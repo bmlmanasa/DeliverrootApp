c7ba54159e995ad15b1d46a07a44d5b64bd2dd63
bmlmanasa <grandhi.manasa.19cs@bmu.edu.in>
2021-11-23 22:35:46 +0530

    Initial commit with cart and products display.

---
+++ app/src/main/java/com/example/deliverroot/sampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.deliverroot;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class sampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
